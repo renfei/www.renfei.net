7ddf2b7a83ad2be44ead5c06682d796717e6b2f7
RenFei <i@renfei.net>
2020-11-02 12:53:38 +0800

    fix: 关闭CSRF，POST请求被拦截问题

--- src/main/java/net/renfei/config/SecurityConfig.java
+++ src/main/java/net/renfei/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/private/**").authenticated()
