a2b9d6e0943e4ffe9ffb06312c19b7fee1c03370
RenFei <i@renfei.net>
2020-11-05 15:54:05 +0800

    config swager

--- src/main/java/net/renfei/config/Swagger3Config.java
+++ src/main/java/net/renfei/config/Swagger3Config.java
@@ -2,7 +2,6 @@ package net.renfei.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -11,10 +10,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class Swagger3Config implements WebMvcConfigurer {
     private final RenFeiConfig renFeiConfig;
 
